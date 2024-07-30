89af149f3020ea32e09ebdbed5e9779d26e2eb04
champl0o <roman.podilka@elopage.com>
2024-07-30 11:20:58 +0300

    Update gemspec, fix versioning

--- lib/globalize/versioning.rb
+++ lib/globalize/versioning.rb
@@ -9,19 +9,19 @@ Globalize::ActiveRecord::ActMacro.module_eval do
   def setup_translates_with_versioning!(options)
     setup_translates_without_versioning!(options)
 
-    if options[:versioning]
-      include Globalize::Versioning::InstanceMethods
+    return unless options[:versioning]
 
-      # hard-coded for now
-      class_attribute :versioning_gem, :instance_accessor => false
-      self.versioning_gem = :paper_trail
+    include Globalize::Versioning::InstanceMethods
 
-      ::ActiveRecord::Base.extend(Globalize::Versioning::PaperTrail)
-      if options[:versioning].is_a?(Hash)
-        translation_class.has_paper_trail(options[:versioning][:options])
-      else
-        translation_class.has_paper_trail
-      end
+    # hard-coded for now
+    class_attribute :versioning_gem, instance_accessor: false
+    self.versioning_gem = :paper_trail
+
+    ::ActiveRecord::Base.extend(Globalize::Versioning::PaperTrail)
+    if options[:versioning].is_a?(Hash)
+      translation_class.has_paper_trail(options[:versioning][:options])
+    elsif options[:versioning] == :paper_trail
+      translation_class.has_paper_trail
     end
   end
 
--- lib/globalize/versioning/paper_trail.rb
+++ lib/globalize/versioning/paper_trail.rb
@@ -5,9 +5,9 @@ module Globalize
     module PaperTrail
       # At present this isn't used but we may use something similar in paper trail
       # shortly, so leaving it around to reference easily.
-      #def versioned_columns
-        #super + self.class.translated_attribute_names
-      #end
+      # def versioned_columns
+      # super + self.class.translated_attribute_names
+      # end
     end
   end
 end
@@ -21,24 +21,4 @@ ActiveRecord::Base.class_eval do
     alias_method :has_paper_trail_without_globalize, :has_paper_trail
     alias_method :has_paper_trail, :has_paper_trail_with_globalize
   end
-end
-
-# to handle different versions of paper_trail
-version_class = PaperTrail::VERSION.is_a?(String) ? Version : PaperTrail::Version
-
-version_class.class_eval do
-
-  before_save do |version|
-    version.locale = Globalize.locale.to_s
-  end
-
-  def self.for_this_locale
-    where :locale => Globalize.locale.to_s
-  end
-
-  def sibling_versions_with_locales
-    sibling_versions_without_locales.for_this_locale
-  end
-  alias_method :sibling_versions_without_locales, :sibling_versions
-  alias_method :sibling_versions, :sibling_versions_with_locales
 end
